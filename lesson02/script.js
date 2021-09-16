429c88dbb47a0d18d348e666a49c84c1d1b5e076
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-09-16 09:29:33 -0600

    added content

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,2 +1,12 @@
-console.log('Hello, I hope you are happy today...')
+function addToList() {
+    let newItem = document.querySelector('#newItem').value;
+    let li = document.createElement('li');
+    li.innerHTML = newItem;
 
+    document.querySelector('#itemList').appendChild(li);
+}
+
+window.onload = (event) => {
+    console.log('page is loaded');
+    document.getElementById('demo').addEventListener('click', addToList);    
+}
