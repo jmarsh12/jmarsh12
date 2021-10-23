f9d3a3c71a33c8c916b852a206d0c0a8dde484d7
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-10-23 12:41:29 -0600

    refined lesson06

--- main_site/lesson06/script.js
+++ main_site/lesson06/script.js
@@ -15,3 +15,22 @@ for (i = 0; i < coll.length; i++) {
 
 document.getElementById("last-updated").textContent =
     "Last Updated: " + document.lastModified;
+
+function calculate_wind_chill() {
+
+    var t = parseInt(document.getElementById('high').innerHTML)
+    var s = parseInt(document.getElementById('wind-speed').innerHTML)
+    console.log(t)
+
+    var wind_chill = null
+
+    if (t <= 50 && s > 3){
+        wind_chill = Math.round(35.74+0.6215*t - 35.75 * Math.pow(s, 0.16)+0.4275*t*Math.pow(s, 0.16))}
+
+    else {
+        wind_chill = 'N/A'
+    }
+    document.getElementById('wind-chill').innerHTML = wind_chill
+}
+
+calculate_wind_chill()
